409396ccabfb7f9dac9f73f099dfc7db5897d37c
Mariana Alvares <mariana11areal@hotmail.com>
2022-03-01 20:12:43 -0300

    Validações nos campos
    - Instalação de validador de CPF;
    - Validação de obrigatoriedade de alguns campos;

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,18 @@ import InputMask from "react-input-mask";
 function Form(props) {
   const [mask, setMask] = React.useState("(99) 99999-9999");
   const {
-    values: { nome, telefone },
+    values: {
+      nome,
+      telefone,
+      email,
+      cpf,
+      dataNascimento,
+      rg,
+      orgaoEmissor,
+      nacionalidade,
+      endereco,
+      numero,
+    },
     errors,
     touched,
     handleChange,
@@ -13,10 +24,10 @@ function Form(props) {
     setFieldTouched,
   } = props;
 
-  const change = (nome, e) => {
+  const change = (element, e) => {
     e.persist();
     handleChange(e);
-    setFieldTouched(nome, true, false);
+    setFieldTouched(element, true, false);
   };
   return (
     <Grid container justifyContent="center">
@@ -52,15 +63,21 @@ function Form(props) {
             <Grid item lg={3}>
               <InputMask
                 mask="99/99/9999"
-                maskChar={
-                  null
-                } /*onChange={formik.handleChange} value={formik.values.document}*/
+                maskChar={null}
+                onChange={change.bind(null, "dataNascimento")}
+                value={dataNascimento}
               >
                 {() => (
                   <TextField
                     id="dataNascimento"
                     name="dataNascimento"
-                    label="Data Nascimento"
+                    helperText={
+                      touched.dataNascimento ? errors.dataNascimento : ""
+                    }
+                    error={
+                      touched.dataNascimento && Boolean(errors.dataNascimento)
+                    }
+                    label="Data nascimento"
                     fullWidth
                     size="small"
                   />
@@ -101,20 +118,38 @@ function Form(props) {
               <TextField
                 id="email"
                 name="email"
+                helperText={touched.email ? errors.email : ""}
+                error={touched.email && Boolean(errors.email)}
                 label="E-mail"
+                onChange={change.bind(null, "email")}
+                value={email}
                 fullWidth
                 size="small"
               />
             </Grid>
-            
+
             <Grid item lg={4}>
-              <TextField id="rg" name="rg" label="RG" fullWidth size="small" />
+              <TextField
+                id="rg"
+                name="rg"
+                helperText={touched.rg ? errors.rg : ""}
+                error={touched.rg && Boolean(errors.rg)}
+                label="RG"
+                onChange={change.bind(null, "rg")}
+                value={rg}
+                fullWidth
+                size="small"
+              />
             </Grid>
             <Grid item lg={4}>
               <TextField
                 id="orgaoEmissor"
                 name="orgaoEmissor"
+                helperText={touched.orgaoEmissor ? errors.orgaoEmissor : ""}
+                error={touched.orgaoEmissor && Boolean(errors.orgaoEmissor)}
                 label="Orgão emissor"
+                onChange={change.bind(null, "orgaoEmissor")}
+                value={orgaoEmissor}
                 fullWidth
                 size="small"
               />
@@ -122,14 +157,16 @@ function Form(props) {
             <Grid item lg={4}>
               <InputMask
                 mask="999.999.999-99"
-                maskChar={
-                  null
-                } /*onChange={formik.handleChange} value={formik.values.document}*/
+                maskChar={null}
+                onChange={change.bind(null, "cpf")}
+                value={cpf}
               >
                 {() => (
                   <TextField
                     id="cpf"
                     name="cpf"
+                    helperText={touched.cpf ? errors.cpf : ""}
+                    error={touched.cpf && Boolean(errors.cpf)}
                     label="CPF"
                     fullWidth
                     size="small"
@@ -159,7 +196,11 @@ function Form(props) {
               <TextField
                 id="endereco"
                 name="endereco"
+                helperText={touched.endereco ? errors.endereco : ""}
+                error={touched.endereco && Boolean(errors.endereco)}
                 label="Endereço"
+                onChange={change.bind(null, "endereco")}
+                value={endereco}
                 fullWidth
                 size="small"
               />
@@ -168,7 +209,11 @@ function Form(props) {
               <TextField
                 id="numero"
                 name="numero"
+                helperText={touched.numero ? errors.numero : ""}
+                error={touched.numero && Boolean(errors.numero)}
                 label="Número"
+                onChange={change.bind(null, "numero")}
+                value={numero}
                 fullWidth
                 size="small"
               />
@@ -204,7 +249,11 @@ function Form(props) {
               <TextField
                 id="nacionalidade"
                 name="nacionalidade"
+                helperText={touched.nacionalidade ? errors.nacionalidade : ""}
+                error={touched.nacionalidade && Boolean(errors.nacionalidade)}
                 label="Nacionalidade"
+                onChange={change.bind(null, "nacionalidade")}
+                value={nacionalidade}
                 fullWidth
                 size="small"
               />
--- src/components/Validations.js
+++ src/components/Validations.js
@@ -2,14 +2,42 @@ import React from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import Form from "./Form";
+import { cpf } from "cpf-cnpj-validator";
 
 function InputForm(props) {
   const validationSchema = Yup.object({
     nome: Yup.string().required("Nome é obrigatório"),
-    telefone: Yup.string().required("Telefone é obrigatório").min(14, "Insira um telefone válido").max(15, "Insira um telefone válido")
+    dataNascimento: Yup.string()
+      .required("Data de Nascimento é obrigatório")
+      .min(10, "Insira uma data válida"),
+    telefone: Yup.string()
+      .required("Telefone é obrigatório")
+      .min(14, "Insira um telefone válido!")
+      .max(15, "Insira um telefone válido!"),
+    email: Yup.string()
+      .required("E-mail é obrigatório")
+      .email("Insira um e-mail válido"),
+    rg: Yup.string().required("RG é obrigatório"),
+    orgaoEmissor: Yup.string().required("Orgão emissor é obrigatório"),
+    cpf: Yup.string()
+      .required("CPF é obrigatório")
+      .test("validCpf", "Insira um CPF válido", (value) => cpf.isValid(value)),
+    endereco: Yup.string().required("Endereço é obrigatório"),
+    numero: Yup.string().required("Número é obrigatório"),
+    nacionalidade: Yup.string().required("Nacionalidade é obrigatório"),
   });
-  const values = {nome: "", telefone: ""};
-  const classes = props;
+  const values = {
+    nome: "",
+    telefone: "",
+    email: "",
+    cpf: "",
+    dataNascimento: "",
+    rg: "",
+    endereco: "",
+    orgaoEmissor: "",
+    nacionalidade: "",
+    numero: "",
+  };
 
   return (
     <>
